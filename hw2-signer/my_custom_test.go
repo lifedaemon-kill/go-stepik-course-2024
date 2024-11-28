415486e717af273d6689f34ac8e14786045253be
Ruslan <noname0263@gmail.com>
2024-11-28 16:53:14 +0900

    add my test for tracing only one func

---
+++ hw2-signer/my_custom_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSingleHash(t *testing.T) {
+	inn := [2]int{0, 1}
+	out := make(chan interface{}, 1)
+	in := make(chan interface{}, 1)
+
+	go SingleHash(in, out)
+
+	out <- inn[0]
+	out <- inn[1]
+
+	close(out)
+	for e := range in {
+		fmt.Println(e)
+	}
+}
+func TestExecutePipelineSingleHash(t *testing.T) {
+	inn := [2]int{0, 1}
+
+	myFlowJobs := []job{
+		job(func(in, out chan interface{}) {
+			for e := range inn {
+				out <- e
+			}
+		}),
+		job(SingleHash),
+		job(func(in, out chan interface{}) {
+			for range in {
+				fmt.Println(<-in)
+			}
+		}),
+	}
+
+	ExecutePipeline(myFlowJobs...)
+}
